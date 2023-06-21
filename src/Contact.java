ea877addc63bb78f8cc2b5ffc072133964412bd5
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-21 21:21:38 +0300

    is almost rdy

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,27 @@
 public class Contact {
-    // TODO vegyél fel egy név és egy telefonszám fieldet, írj konstruktort és gettereket!
+    // TODO vegyél fel egy név és egy telefonszám fieldet,
+    //  írj konstruktort és gettereket!
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/PhoneBookManager.java
+++ src/PhoneBookManager.java
@@ -17,35 +17,87 @@ public class PhoneBookManager {
         while (running) {
             printMenu(); // TODO valósítsd meg a printMenu() metódust!
             int choice; // TODO olvasd be a felhasználótól, mit szeretne csinálni!
+            Scanner scanner = new Scanner(System.in);
+            choice = scanner.nextInt();
             scanner.nextLine(); // vajon miért kell ez a sor?
-            // TODO a megadott szám alapján hívd meg a megfelelő metódust vagy lépj ki!
+            // TODO a megadott szám alapján hívd meg a megfelelő metódust
+            //  vagy lépj ki!
+            switch (choice) {
+                case 1 -> addContact();
+                case 2 -> removeContact();
+                case 3 -> searchContact();
+                case 4 -> listContacts();
+                case 5 -> System.out.println("Exit...");
+                default -> System.out.println("Invalid choice");
+            }
         }
         scanner.close();
     }
 
     private void printMenu() {
-        // TODO Írd ki az alábbi menüsort:
-        //  0. List contacts
-        //  1. Add contact
-        //  2. Search contact
-        //  3. Remove contact
-        //  4. Exit
+        System.out.println("Phone Book Menu");
+        System.out.println("1. Add contact");
+        System.out.println("2. Remove contact");
+        System.out.println("3. Search contact");
+        System.out.println("4. Display all contacts");
+        System.out.println("5. Exit");
+        System.out.print("Enter your choice: ");
     }
 
     private void listContacts() {
         // TODO Szépen formázva írd ki a telefonkönyv összes bejegyzését!
+        System.out.println("Phone book entries: ");
+
+        for (Contact contact : phoneBook.values()) {
+            String name = contact.getName();
+            String phoneNumber = contact.getPhoneNumber();
+
+            System.out.println("Name: " + name );
+            System.out.println("Phone number: " + phoneNumber);
+            System.out.println();
+        }
+
+
     }
 
     private void addContact() {
+         Map<String,String> phoneBook = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
         // TODO olvasd be egy változóba a kontakt nevét
+        System.out.println("Contact name: ");
+        String name = scanner.nextLine();
+
         // TODO olvasd be egy változóba a kontakt telefonszámát
+        System.out.println("Contact phone number: ");
+        String phoneNumber = scanner.nextLine();
         // TODO példányosíts ezekből az adatokból egy Contact-ot
         // TODO tedd bele ezeket az infókat a mapbe:
         //      mi lesz a kulcs és mi az érték?
+        Contact contact = new Contact(name, phoneNumber);
+        phoneBook.put(name,phoneNumber);
+        System.out.println("Contact added successfully!");
+        System.out.println("Name: " + name);
+        System.out.println("Phone number: " + phoneNumber);
+
     }
 
     private void searchContact() {
         // TODO valósítsd meg a név alapján való keresést
+        System.out.print("Enter name to search: ");
+        String searchName = scanner.nextLine();
+        if(phoneBook.containsKey(searchName)){
+            Contact contact = phoneBook.get(searchName);
+            String name = contact.getName();
+            String phoneNumber = contact.getPhoneNumber();
+
+            System.out.println("Contact found: ");
+            System.out.println("Name: " + name);
+            System.out.println("Phone number: " + phoneNumber);
+        }else{
+            System.out.println("Contact not found! ");
+        }
+
+
         //  ha nem találod meg az adott kontaktot, jelezd
         //  azt is a felhasználónak!
     }
@@ -54,6 +106,15 @@ public class PhoneBookManager {
         // TODO valósítsd meg a név alapján való törlést
         //  ha nem találod meg az adott kontaktot, jelezd
         //  azt is a felhasználónak!
+        System.out.println("Enter name to remove: ");
+        String removeName = scanner.nextLine();
+        if(phoneBook.containsKey(removeName)){
+            phoneBook.remove(removeName);
+            System.out.println("Contact removed successfully!");
+        }else{
+            System.out.println("Contact not found! ");
+        }
+
     }
 
 }
